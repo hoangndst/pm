a34e2647456cb3a7c14d0d0087a2e1b40da4aac8
hoangndst <hoangndst@outlook.com>
2022-10-04 15:10:08 +0700

    kien

---
+++ pm-client/src/components/TableTask.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { TextField } from '@fluentui/react/lib/TextField';
+import { Toggle } from '@fluentui/react/lib/Toggle';
+import { Announced } from '@fluentui/react/lib/Announced';
+import { DetailsList, DetailsListLayoutMode, Selection, SelectionMode, IColumn } from '@fluentui/react/lib/DetailsList';
+import { MarqueeSelection } from '@fluentui/react/lib/MarqueeSelection';
+import { mergeStyleSets } from '@fluentui/react/lib/Styling';
+import { TooltipHost } from '@fluentui/react';
+
+type IDetail
+
+
+export const TableTask = () => {
+  return (
+    <>
+    </>
+  )
+}
