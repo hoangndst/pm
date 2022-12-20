82a854f9ec6bbed260c3358159515a4e91d6e79d
lethicamnhung1 <20021408@vnu.edu.vn>
2022-12-20 14:16:27 +0700

    d

--- pm-server/middleware/authJWT.js
+++ pm-server/middleware/authJWT.js
@@ -8,7 +8,7 @@ const secret = databaseConfig.PM_SECRET
 const verifyToken = (req, res, next) => {
   let authorization = req.headers["authorization"]
   const token = authorization
-  // console.log(token)
+
   if (!token) {
     return res.status(403).send({ message: "No token provided!" })
   }
@@ -17,7 +17,6 @@ const verifyToken = (req, res, next) => {
       return res.status(401).send({ message: "Unauthorized!" })
     }
     req.userId = decoded.id
-    // console.log("req.userId: " + req.userId)
     next()
   })
 }
